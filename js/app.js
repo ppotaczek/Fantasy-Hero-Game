aa3da30113f94bee525bc13c00673de9690cabe5
ppotaczek <ppotaczek8@gmail.com>
2017-02-21 11:41:07 +0100

    Shooting enemy

--- js/app.js
+++ js/app.js
@@ -5,21 +5,24 @@ $(document).ready(function() {
   var enemyPropertiesMummy = {
     class: "enemyMummy",
     class2: "enemyMummy2",
+    destroyClass: "enemyMummy",
     moveStart: -1107,
     moveStep: 123,
     attackStart: -861,
     experience: 10,
-    directionAttack: "406px",
+    directionAttack: "-406px",
     height: 167,
     delay: 2000,
     life: 1,
     randomY: 550,
-    frames: 10
+    frames: 10,
+    strength: 1
   }
 
   var enemyPropertiesBehemoth = {
     class: "enemyBehemoth",
     class2: "enemyBehemoth2",
+    destroyClass: "enemyBehemoth2",
     moveStart: -2430,
     moveStep: 270,
     attackStart: -1620,
@@ -29,7 +32,59 @@ $(document).ready(function() {
     delay: 4000,
     life: 3,
     randomY: 450,
-    frames: 7
+    frames: 7,
+    strength: 3
+  }
+
+  var enemyPropertiesEnt = {
+    class: "enemyEnt",
+    class2: "enemyEnt2",
+    destroyClass: "enemyEnt2",
+    moveStart: -1512,
+    moveStep: 216,
+    attackStart: -1296,
+    experience: 50,
+    directionAttack: "-430px",
+    height: 215,
+    delay: 3000,
+    life: 4,
+    randomY: 450,
+    frames: 7,
+    strength: 2
+  }
+  var enemyPropertiesMedusa = {
+    class: "enemyMedusa",
+    class2: "enemyMedusa2",
+    destroyClass: "enemyMedusa2",
+    moveStart: -1169,
+    moveStep: 167,
+    attackStart: -1002,
+    experience: 30,
+    directionAttack: "-360px",
+    height: 180,
+    delay: 5000,
+    life: 2,
+    randomY: 520,
+    frames: 6,
+    strength: 2,
+    classBullet: "enemyMedusaBullet"
+  }
+  var enemyPropertiesCyclop = {
+    class: "enemyCyclop",
+    class2: "enemyCyclop2",
+    destroyClass: "enemyCyclop2",
+    moveStart: -1256,
+    moveStep: 157,
+    attackStart: -1099,
+    experience: 130,
+    directionAttack: "-414px",
+    height: 207,
+    delay: 7000,
+    life: 4,
+    randomY: 500,
+    frames: 7,
+    strength: 4,
+    classBullet: "enemyCyclopBullet"
   }
 
 //Movement system and limit
@@ -102,13 +157,50 @@ $(document).ready(function() {
             {duration: 2000, easing: "linear", complete: function(){
               enemy.pause()
                 .addClass(enemyProperties.class2);
+              if (enemy.attr("data-life") < 1){
+                return false;
+              }
               setTimeout(function(){
+                if (enemy.attr("data-life") < 1){
+                  return false;
+                }
                 enemy.removeClass(enemyProperties.class2)
                   .resume()
-                  .animate({left: "-150px", top: randomDirection},
-                    {duration: 12000, easing: "linear", complete: function(){$(this).remove();}});
-              }, 1000);
+                  .animate({left: "-250px", top: randomDirection},
+                    {duration: 13000, easing: "linear", complete: function(){$(this).remove();}});
+              }, 500);
             }})
+        }
+      }
+      else if (enemyType == "ent"){
+        var enemyProperties = enemyPropertiesEnt;
+        function enemyAnimate(enemy, randomDirection){
+          enemy.animate({right: "50px"},
+            {duration: 6000, easing: "linear"})
+            .animate({left: "-150px", top: randomDirection},
+            {duration: 25000, easing: "linear", complete: function(){$(this).remove();}});
+        }
+      }
+      else if (enemyType == "medusa"){
+        var enemyProperties = enemyPropertiesMedusa;
+        function enemyAnimate(enemy, randomDirection){
+          enemy.addClass("shooter")
+            .animate({right: "20px"},
+            {duration: 2000, easing: "linear", complete: function(){
+              clearInterval($(this).attr("data-interval"));
+              enemyShoot(enemy, enemyProperties, 4);
+            }});
+        }
+      }
+      else if (enemyType == "cyclop"){
+        var enemyProperties = enemyPropertiesCyclop;
+        function enemyAnimate(enemy, randomDirection){
+          enemy.addClass("shooter")
+            .animate({right: "20px"},
+            {duration: 3000, easing: "linear", complete: function(){
+              clearInterval($(this).attr("data-interval"));
+              enemyShoot(enemy, enemyProperties, 20);
+            }});
         }
       }
 
@@ -127,6 +219,9 @@ $(document).ready(function() {
 
   function intervalBGAnimation(enemyProperties, enemy){
     var animationStep = 1;
+    if (enemy.hasClass("shooter")){
+      return false;
+    }
     var interval = setInterval(function(){
 
       var heroPosX = parseInt(sprite.css("left"))+43;
@@ -134,12 +229,6 @@ $(document).ready(function() {
       var thisX = parseInt(enemy.css("left"));
       var thisY = parseInt(enemy.css("top"));
       var hit = sprite.hasClass("hit");
-
-      //Checking bullet
-      if ($("div.bullet").length){
-        killEnemy($("div.bullet"), enemyProperties, enemy, thisX, thisY);
-      }
-
       enemy.attr("data-interval", interval)
         .css("background-position", (enemyProperties.moveStart+(animationStep*enemyProperties.moveStep))+"px 0");
       animationStep++;
@@ -157,14 +246,7 @@ $(document).ready(function() {
       }
 
       if (thisY <= heroPosY && thisY + height >= heroPosY-100 && conditionX){
-        console.log(dirAtt);
-        sprite.addClass("hit");
-
-        for (var i = 0; i < 39; i++) {
-          sprite.toggle(80);
-          i++;
-        }
-        setTimeout(function(){sprite.removeClass("hit");}, 4000);
+        hitHero(enemyProperties.strength);
         clearInterval(enemy.attr("data-interval"));
         enemy.pause()
           .addClass(enemyProperties.class2);
@@ -197,6 +279,67 @@ $(document).ready(function() {
       .addClass("enemy")
   }
 
+  function enemyShoot(enemy, propertiesObj, number){
+    var enPosY = parseInt(enemy.css("top"));
+    var enPosX = parseInt(enemy.css("left"));
+    var iteration = 0;
+    var shootInterval = setInterval(function(){
+      var animateIteration = 0;
+      var enemyShootAnimation = setInterval(function(){
+        if (!enemy.hasClass("enemy")){
+          clearInterval(shootInterval);
+          clearInterval(enemyShootAnimation);
+          return false;
+        }
+        enemy.addClass(propertiesObj.class2);
+        animateIteration++;
+        enemy.css("background-position", (propertiesObj.moveStart+(animateIteration*propertiesObj.moveStep))+"px 0");
+        if (animateIteration > propertiesObj.frames){
+          clearInterval(enemyShootAnimation);
+          var heroX = parseInt(sprite.css("left"));
+          var heroY = parseInt(sprite.css("top"));
+          var enemyBullet = $("<div>")
+          .addClass("enemyBullet")
+          .addClass(propertiesObj.classBullet)
+          .css("top", enPosY+70)
+          .css("left", enPosX);
+          container.append(enemyBullet);
+          enemyBullet.animate({left: heroX-10, top: heroY+30},
+          {duration: 2000, easing: "linear", complete: function(){$(this).remove();}});
+          ++iteration;
+
+          var checkHeroHit = setInterval(function(){
+            var enemyBulletX = parseInt(enemyBullet.css("left"));
+            var enemyBulletY = parseInt(enemyBullet.css("top"));
+            var heroX = parseInt(sprite.css("left"));
+            var heroY = parseInt(sprite.css("top"));
+
+            var conditionX = enemyBulletX > heroX && enemyBulletX < heroX+60;
+            var conditionY = enemyBulletY > heroY && enemyBulletY < heroY+100;
+            var hit = sprite.hasClass("hit");
+
+            if (conditionX && conditionY && !hit){
+              clearInterval(checkHeroHit);
+              enemyBullet.remove();
+              hitHero(propertiesObj.strength);
+
+            }
+          }, 20);
+
+          if (iteration >= number){
+            clearInterval(shootInterval);
+            iteration = 0;
+            enemy.removeClass(propertiesObj.class2)
+              .animate({left: "-150px", top: heroY},
+                {duration: 15000, easing: "linear", complete: function(){$(this).remove();}})
+              .removeClass("shooter");
+            intervalBGAnimation(propertiesObj, enemy);
+          }
+        }}, 100)
+
+    }, 2000)
+  }
+
   function shoot(posY, posX){
     sprite.animate({top: "+=0"},
     {duration: 200, complete:
@@ -219,7 +362,8 @@ $(document).ready(function() {
           .css("top", posY+30)
           .css("left", posX+100);
           container.append(bullet);
-          bullet.animate({left: "1000px"},
+          killEnemy(bullet);
+          bullet.animate({left: posX+1000},
           {duration: 1000, easing: "linear", complete: function(){$(this).remove();}});
           sprite.css("background-position", "0 0")
           .removeClass("spriteShoot");
@@ -227,62 +371,115 @@ $(document).ready(function() {
     });
   }
 
-  function hitEnemy(hit, propertiesObj){
+  function hitHero(power){
+    var actualLifes = ($("#lifes").text())-power;
+    $("#lifes").text(actualLifes);
+    sprite.addClass("hit");
+    for (var i = 0; i < 39; i++) {
+      sprite.toggle(80);
+      i++;
+    }
+    setTimeout(function(){sprite.removeClass("hit");}, 4000);
+  }
+
+  function addExperience(amount){
+    var actualExp = parseInt($("#exp").text());
+    var newExp = actualExp+parseInt(amount);
+    var actualMana = parseInt($("#mana").text());
+    var newMana = actualMana+2;
+    $("#exp").text(newExp);
+    $("#mana").text(newMana);
+  }
+
+  function hitEnemy(hit, typeProperties){
     var iteration = 0;
     hit.pause()
-      .addClass(propertiesObj.class2)
+      .addClass(typeProperties.destroyClass)
     var hitAnimation = setInterval(function(){
+      if (hit.attr("data-life") < 1){
+        return false;
+      }
       iteration++;
-      hit.css("background-position", propertiesObj.attackStart+(iteration*propertiesObj.step)+"px "+(-propertiesObj.height)*4);
-      if (iteration == propertiesObj.frames-1){
+      hit.css("background-position", typeProperties.attackStart+(iteration*typeProperties.moveStep)+"px "+typeProperties.height*(-4)+"px");
+      if (iteration == typeProperties.frames-1){
         clearInterval(hitAnimation);
       }
     }, 80)
-      // .css("background-position", "0 "+ -(parseInt(hit.css("height"))*4)+"px");
 
     setTimeout(function(){
       hit.resume()
-        .removeClass("enemyBehemoth2");
-      intervalBGAnimation(enemyPropertiesBehemoth, hit);
-
+        .removeClass(typeProperties.destroyClass);
+      intervalBGAnimation(typeProperties, hit);
     }, 500)
   }
-  // function killAnimation(toKill){
-  //   var iteration = 0;
-  //   var killAnimation = setInterval(function(){
-  //     iteration++;
-  //     toKill.css("background-position", (-1107+(iteration*123))+"px 167px");
-  //     if (iteration == toKill.attr("data-frames")-1){
-  //       clearInterval(killAnimation);
-  //     }
-  //   }, 50)
-  // }
 
-  function killEnemy(fireball, enemyProperties, enemy, enemyX, enemyY){
-    var fireballPosY = parseInt(fireball.css("top"));
-    var fireballPosX = parseInt(fireball.css("left"));
+  function killAnimation(toKill, typeProperties){
+    var iteration = 0;
+    var killAnimation = setInterval(function(){
+      iteration++;
+      toKill.addClass(typeProperties.destroyClass)
+        .css("background-position", (-typeProperties.moveStep*(typeProperties.frames-1))+(iteration*typeProperties.moveStep)+"px "+typeProperties.height+"px")
 
-    var checkInterval = setInterval(function(){
-      if (fireballPosY >= enemyY && fireballPosY <= enemyY+enemyProperties.height && fireballPosX >= enemyX-60 && fireballPosX <= enemyX+20){
-        clearInterval(checkInterval);
-        fireball.remove();
-        enemy.attr("data-life", parseInt(enemy.attr("data-life")-1));
-        clearInterval($(this).attr("data-interval"));
-        if (enemy.attr("data-life") > 0){
-          hitEnemy(enemy, enemyProperties);
+      if (iteration == typeProperties.frames-1){
+        clearInterval(killAnimation);
+        clearInterval(toKill.attr("data-interval"));
+      }
+    }, 50)
+  }
+
+  function killEnemy(fireball){
+    var intervalCheck = setInterval(function(){
+      var fireballPosY = parseInt(fireball.css("top"));
+      var fireballPosX = parseInt(fireball.css("left"));
+      var enemies = $(".enemy");
+
+      enemies.each(function(){
+        var thisTop = parseInt($(this).css("top"));
+        var thisLeft = parseInt($(this).css("left"));
+        var $this = $(this);
+        var typeProperties = null;
+
+        if ($this.hasClass("enemyMummy")){
+          typeProperties = enemyPropertiesMummy;
+        }
+        else if ($this.hasClass("enemyBehemoth")){
+          typeProperties = enemyPropertiesBehemoth;
+        }
+        else if ($this.hasClass("enemyEnt")){
+          typeProperties = enemyPropertiesEnt;
+        }
+        else if ($this.hasClass("enemyMedusa")){
+          typeProperties = enemyPropertiesMedusa;
+        }
+        else if ($this.hasClass("enemyCyclop")){
+          typeProperties = enemyPropertiesCyclop;
         }
 
-        else if (enemy.attr("data-life") < 1){
-          enemy.stop()
-            .removeClass("enemy");
-          //killAnimation($this);
-          enemy.stop();
-          return false;
+        if (fireballPosY+40 >= thisTop+30 && fireballPosY <= thisTop+typeProperties.height-30 && fireballPosX >= thisLeft-20 && fireballPosX <= thisLeft+20){
+          clearInterval(intervalCheck);
+          fireball.remove();
+          $this.attr("data-life", parseInt($this.attr("data-life")-1));
+          clearInterval($(this).attr("data-interval"));
+          if ($this.attr("data-life") > 0){
+            hitEnemy($this, typeProperties);
+          }
+
+          else if ($this.attr("data-life") < 1){
+            $this.stop()
+              .removeClass("enemy");
+            killAnimation($this, typeProperties);
+            clearInterval($(this).attr("data-interval"));
+            addExperience($this.attr("data-experience"));
+            $this.stop();
+          }
         }
-      };
-    }, 20)
+      });
+    }, 20);
 
   }
   moveSystem(30, 890, 70, 570);
-  createEnemy(2, "behemoth");
+  //createEnemy(5, "ent");
+  //createEnemy(25, "mummy");
+  createEnemy(5, "cyclop");
+  createEnemy(5, "medusa");
 });
